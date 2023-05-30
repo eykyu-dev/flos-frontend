69fb068a006570cfa87650691fa9b1759c37be55
Dustin Santoso <dustin.santoso025@gmail.com>
2023-05-30 15:46:43 -0700

    made box :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+
 import NavBar from './components/navbar'
+import HomeBlock from './components/homeblock'
+import './App.css'
 
 function App() {
 
   return (
-    <div>
+    <div className='home-page' >
       <NavBar></NavBar>
+      <HomeBlock></HomeBlock>
     </div>
   )
 }
