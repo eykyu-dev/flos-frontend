c5a7313fad18b63b52b96b1ae7705a83b2b80788
Dustin Santoso <dustin.santoso025@gmail.com>
2023-05-30 16:58:38 -0700

    added graphic

--- src/components/homeblock.tsx
+++ src/components/homeblock.tsx
@@ -1,9 +1,15 @@
 import './homeblock.css'
+import PlantImage from '../../src/unsplash_cLaaxa4DSnc-removebg-preview 1.png'
 
 function HomeBlock(){
 return(
     <div className="outer-block">
         <div className="home-text">
+            hello
+        </div>
+        <div className='graphic'>
+        <div className="circle"></div>
+        <img src={PlantImage}></img>
         </div>
     </div>
 )
